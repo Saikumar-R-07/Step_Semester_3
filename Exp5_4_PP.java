f89a4388ae7db39f38ea77bfe09ff3d72100ae08
Saikumar-R-07 <saikumar23211@gmail.com>
2026-09-12 11:04:01 +0530

    Implement PatientProfile class with basic functionality

---
+++ Exp5_4_PP.java
@@ -0,0 +1,51 @@
+public class Problem4 {
+    public static void main(String[] args) {
+        System.out.println(new PatientProfile("Arjun Iyer").getPatientId());
+        System.out.println(new PatientProfile("MT2026-0142", "Arjun Iyer").getPatientId());
+        
+        PatientProfile p = new PatientProfile();
+        p.setPatientId("MT2026-0142");
+        p.setPatientId("HACKED-0000");
+        System.out.println(p.getPatientId());
+    }
+}
+
+class PatientProfile {
+    private String patientId;
+    private String name;
+    private boolean discharged;
+    private String lockerPinHash;
+
+    public PatientProfile() {
+        this(null, null);
+    }
+
+    public PatientProfile(String name) {
+        this(null, name);
+    }
+
+    public PatientProfile(String patientId, String name) {
+        this.patientId = patientId;
+        this.name = name;
+        this.discharged = false;
+    }
+
+    public String getPatientId() { return patientId; }
+    public void setPatientId(String id) {
+        if (this.patientId == null) {
+            this.patientId = id;
+        }
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public boolean isDischarged() { return discharged; }
+    public void setDischarged(boolean discharged) { this.discharged = discharged; }
+
+    public void setLockerPin(String pin) {
+        if (pin != null && pin.matches("\\d{4,6}")) {
+            this.lockerPinHash = String.valueOf(pin.hashCode());
+        }
+    }
+}
